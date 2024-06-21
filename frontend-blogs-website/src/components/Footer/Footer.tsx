90c6365e9f5cb72678e01edab5384611e950e285
nerm1k <nerm1k@mail.ru>
2024-06-21 09:49:46 +0500

    added footer

--- frontend-blogs-website/src/components/Footer/Footer.tsx
+++ frontend-blogs-website/src/components/Footer/Footer.tsx
@@ -1,9 +1,36 @@
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
     return(
-        <footer>
-            footer
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.footer__item}>
+                    © 2024, Бойко Роман
+                </div>
+                <div className={styles.footer__item}>
+                    <Link to={'./feedback'}>
+                        Техническая поддержка
+                    </Link>
+                </div>
+                <div className={styles.footer__item}>
+                    <div className={styles['footer__img-link']}>
+                        <a href="https://vk.com/nermi">
+                            <img src="./images/vk.png" alt="vk.com/nermi" />
+                        </a>
+                    </div>
+                    <div className={styles['footer__img-link']}>
+                        <a href="https://t.me/persikkkkkkkkkkkkkkkk">
+                            <img src="./images/telegram.png" alt="t.me/persikkkkkkkkkkkkkkkk" />
+                        </a>
+                    </div>
+                    <div className={styles['footer__img-link']}>
+                        <a href="https://github.com/nerm1k">
+                            <img src="./images/github.png" alt="github.com/nerm1k" />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
